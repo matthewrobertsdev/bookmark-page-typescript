bee28c24ac864dc5702d2944f1888922a427f604
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-10-31 21:57:14 -0400

    Start of bookmark-page.  Re-arrangable, colors, buttons.

---
+++ App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import './app.css';
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap
+} from "react-grid-dnd";
+ 
+function Example() {
+  const [items, setItems] = React.useState([1, 2, 3, 4, 5, 6, 7, 8]); // supply your own state
+ 
+  // target id will only be set if dragging from one dropzone to another.
+  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+    const nextState = swap(items, sourceIndex, targetIndex);
+    setItems(nextState);
+  }
+  
+ 
+  return (
+    <div>
+      <ul className='tool-bar'><li className='tool-item rearrange-button'>Rearrange</li>
+      <li className='tool-item delete-button'>Delete</li>
+      <li className='tool-item edit-button'>Edit</li>
+      <li className='tool-item add-button'>Add</li>
+      <li className='tool-item settings-button'>Settings</li>
+      </ul>
+    <GridContextProvider onChange={onChange} style={{cursor: 'default'}}>
+      <br></br><br></br><br></br><br></br><br></br>
+      <GridDropZone
+        id="items"
+        boxesPerRow={2}
+        rowHeight={40}
+        style={{ height: "400px", width: '100%', cursor: 'default', textAlign: 'center'}}
+        //disableDrag={true} //disableDrop={true}
+      >
+        {items.map(item => (
+          <GridItem key={item}>
+            <a 
+              style={{
+                height: "50%",
+                display: 'inline-block',
+                touchAction: 'none',
+                cursor: 'default',
+                textAlign: 'center',
+                borderRadius: '10px',
+                padding: '3px 11px'
+              }}
+              className='link-button'
+            >
+              {item}
+            </a>
+          </GridItem>
+        ))}
+      </GridDropZone>
+    </GridContextProvider>
+    </div>
+  );
+}
+
+export default Example;
---
+++ index.js
@@ -0,0 +1,82 @@
+
+//*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Example from './App';
+import * as serviceWorker from './serviceWorker';
+ReactDOM.render(<Example />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+//*/
+/*
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
+const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+
+class SortableComponent extends Component {
+  state = {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+  };
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(({items}) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
+  };
+  render() {
+    return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} axis='xy'/>;
+  }
+}
+
+render(<SortableComponent/>, document.getElementById('root'));
+
+/*
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
+const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+
+class SortableComponent extends Component {
+  state = {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+  };
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(({items}) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
+  };
+  render() {
+    return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+  }
+}
+
+render(<SortableComponent />, document.getElementById('root'));
+*/
