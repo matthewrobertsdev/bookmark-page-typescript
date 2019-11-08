8e2347e9f4234171622609387b6aa7fd7861eb2e
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-08 14:57:13 -0500

    Refactored to style object function and if statements to within mapping

--- BookmarkGrid.js
+++ BookmarkGrid.js
@@ -2,25 +2,33 @@ import React from 'react';
 import './app.css';
 import { GridContextProvider, GridDropZone, GridItem, swap } from "react-grid-dnd";
 import { connect } from 'react-redux';
-import {setBookmarks, toggleCheckedState, setMode, setUpdatingIndex, setUpdatingName, 
-    setUpdatingURL} from './Actions'
-import {saveBookmarks} from './BookmarkReducer';
+import {
+    setBookmarks, toggleCheckedState, setMode, setUpdatingIndex, setUpdatingName,
+    setUpdatingURL
+} from './Actions'
+import { saveBookmarks } from './BookmarkReducer';
 
+const mapStateToProps = (state) => {
+    return {
+        bookmarks: state.bookmarks, mode: state.mode,
+        checkedArray: state.checkedArray
+    }
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setBookmarks: (bookmarks) => { dispatch(setBookmarks(bookmarks)); },
+        toggleCheckedState: (index) => { dispatch(toggleCheckedState(index)); },
+        setMode: (mode) => { dispatch(setMode(mode)); },
+        setUpdatingIndex: (index) => { dispatch(setUpdatingIndex(index)) },
+        setUpdatingName: (name) => { dispatch(setUpdatingName(name)); },
+        setUpdatingURL: (URL) => { dispatch(setUpdatingURL(URL)) }
+    }
+};
+class UnconnectedBookmarkGrid extends React.Component {
 
-const mapStateToProps = (state) => { return {bookmarks: state.bookmarks, mode: state.mode,
-checkedArray: state.checkedArray} };
-const mapDispatchToProps = (dispatch) => 
-{return { setBookmarks: (bookmarks) => { dispatch(setBookmarks(bookmarks)); },
-toggleCheckedState: (index) => { dispatch(toggleCheckedState(index)); },
-setMode: (mode) => { dispatch(setMode(mode)); },
-setUpdatingIndex:(index)=>{dispatch(setUpdatingIndex(index))},
-setUpdatingName: (name) => { dispatch(setUpdatingName(name)); },
-setUpdatingURL: (URL) => {dispatch(setUpdatingURL(URL))} }};
-class UnconnectedBookmarkGrid extends React.Component{
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.changeIndices=this.changeIndices.bind(this);
+        this.changeIndices = this.changeIndices.bind(this);
     }
 
     // target id will only be set if dragging from one dropzone to another.
@@ -30,80 +38,81 @@ class UnconnectedBookmarkGrid extends React.Component{
         saveBookmarks(nextState);
     }
 
-    render(){
-    return (<GridContextProvider onChange={this.changeIndices} style={{ cursor: 'default', marginLeft: '0px', marginRight: '0px'}}>
-        <GridDropZone id="items" boxesPerRow={1} rowHeight={40}
-            style={{ height: this.getHeightString(), width: '80%', touchAction: this.getTouchActionString(), cursor: 'default', 
-            textAlign: 'center', marginLeft: '10%', marginRight: '10%'}}
-            disableDrag={!this.isRearrangeMode()} disableDrop={!this.isRearrangeMode()} className='bookmark-grid'>
-            {this.getBookmarkGrid()}
-        </GridDropZone>
-    </GridContextProvider>);
+    render() {
+        return (<GridContextProvider onChange={this.changeIndices} style={{ cursor: 'default', marginLeft: '0px', marginRight: '0px' }}>
+            <GridDropZone id="items" boxesPerRow={1} rowHeight={40}
+                style={{
+                    height: this.getHeightString(), width: '80%', touchAction: this.getTouchActionString(), cursor: 'default',
+                    textAlign: 'center', marginLeft: '10%', marginRight: '10%'
+                }}
+                disableDrag={!this.isRearrangeMode()} disableDrop={!this.isRearrangeMode()} className='bookmark-grid'>
+                {this.getBookmarkGrid()}
+            </GridDropZone>
+        </GridContextProvider>);
     }
 
-    getBookmarkGrid(){
-        if (this.props.mode==='rearrange'){
-            return this.props.bookmarks.map((item, index)=>{return <GridItem key={index}>
-                <label style={{
-                    height: "50%", display: 'inline-block', touchAction: 'none',
-                    cursor: 'default', textAlign: 'center', borderRadius: '10px',
-                    padding: '3px 11px', marginTop: '8px'
-                }}
-                    className='link-button'>
-                    {item.name}
-                </label> </GridItem>})
-        } else if (this.props.mode==='edit'||this.props.mode==='update'){
-            return this.props.bookmarks.map((item, index)=>{return <GridItem key={index}>
-                <label style={{
-                    height: "50%", display: 'inline-block', touchAction: 'none',
-                    cursor: 'default', textAlign: 'center', borderRadius: '10px',
-                    padding: '3px 11px', marginTop: '8px'
-                }}
-                    className='link-button' onClick={()=>{this.updateBookmark(index, item)}}>
-                    {item.name}
-                </label> </GridItem>})
-        } else {
-            return this.props.bookmarks.map((item, index)=>{return <GridItem key={index}>
-                {this.addForDelete(index)}
-                <a href={item.URL} style={{
-                    height: "50%", display: 'inline-block', touchAction: 'default',
-                    cursor: 'pointer', textAlign: 'center', borderRadius: '10px',
-                    padding: '3px 11px', marginTop: '8px'
-                }}
-                    className='link-button'>
-                    {item.name}
-                </a> </GridItem>})
-        }
+    getBookmarkGrid() {
+        return this.props.bookmarks.map((item, index) => {
+            if (this.props.mode === 'rearrange') {
+                return <GridItem key={index}>
+                    <label style={this.getGridItemStyleObject('none', 'default')}
+                        className='link-button'>
+                        {item.name}
+                    </label> </GridItem>
+            } else if (this.props.mode === 'edit' || this.props.mode === 'update') {
+                return <GridItem key={index}>
+                    <label style={this.getGridItemStyleObject('none', 'default')}
+                        className='link-button' onClick={() => { this.updateBookmark(index, item) }}>
+                        {item.name}
+                    </label> </GridItem>
+
+            } else {
+                return <GridItem key={index}>
+                    {this.addForDelete(index)}
+                    <a href={item.URL} style={this.getGridItemStyleObject('default', 'pointer')}
+                        className='link-button'>
+                        {item.name}
+                    </a> </GridItem>
+            }
+        })
     }
 
-    getHeightString(){
-        return (this.props.bookmarks.length*40).toString()+'px';
+    getHeightString() {
+        return (this.props.bookmarks.length * 40).toString() + 'px';
     }
 
-    getTouchActionString(){
+    getTouchActionString() {
         return this.isRearrangeMode() ? 'none' : 'default';
     }
 
-    getCursorString(){
+    getCursorString() {
         return this.isRearrangeMode() ? 'default' : 'pointer';
     }
 
-    isRearrangeMode(){
-        return this.props.mode==='rearrange'
+    isRearrangeMode() {
+        return this.props.mode === 'rearrange'
     }
 
-    addForDelete(index){
-        if(this.props.mode==='delete'){
+    addForDelete(index) {
+        if (this.props.mode === 'delete') {
             return <input type='checkbox' checked={this.props.checkedArray[index]}
-            onChange={()=>this.props.toggleCheckedState(index)}className='delete-check-box'></input>
+                onChange={() => this.props.toggleCheckedState(index)} className='delete-check-box'></input>
         }
     }
 
-    updateBookmark(index, bookmark){
+    updateBookmark(index, bookmark) {
         this.props.setMode('update');
         this.props.setUpdatingIndex(index);
         this.props.setUpdatingName(bookmark.name);
         this.props.setUpdatingURL(bookmark.URL);
+    }
+
+    getGridItemStyleObject(touchAction, pointer) {
+        return {
+            height: "50%", display: 'inline-block', touchAction: touchAction,
+            cursor: pointer, textAlign: 'center', borderRadius: '10px',
+            padding: '3px 11px', marginTop: '8px'
+        }
     }
 
 }
