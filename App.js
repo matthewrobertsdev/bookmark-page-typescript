1c2a8432cfb851c4a283e8071c4275f9b30b58f9
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-12-09 10:46:17 -0500

    Changed dark colors and added google it feature.

--- App.js
+++ App.js
@@ -8,10 +8,43 @@ import { connect } from 'react-redux';
  
 const mapStateToProps = (state) => { return {bookmarks: state.bookmarks} };
 class UnconnectedApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {searchText: '', searchEngine: 'Google'};
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
   
+  handleSearchChange(event) {
+    this.setState({searchText: event.target.value});
+  }
+
+  handleSearchEngineChange(event) {
+    this.setState({searchEngine: event.target.value});
+  }
+
+  handleSearch(event) {
+    window.open('http://google.com/search?q='+this.state.searchText, "_self");
+    event.preventDefault();
+  }
  
   render(){ return (<div><ToolBar/><EntryModal/><MoreModal/>
-      <br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <div className='center'>
+        {/*}
+        <select name="searchEngine" id="searchEngine" 
+        className='search-select' value={this.state.searchEngine}>
+          <option value="google">Google</option>
+          <option value="duckduckgo">Duck Duck Go</option>
+          <option value="bing">Bing</option>
+        </select >
+  */}
+        <span className='spacer'></span>
+        <input class='search-input' tabIndex="1" value={this.state.searchText} 
+        onChange={this.handleSearchChange}></input>
+        <span className='spacer'></span>
+        <button onClick={this.handleSearch}>Google</button></div>
       <h1 className='text-on-background'>Bookmarks</h1>
       <br></br>{this.getBookmarkContent()}<br></br><br></br><br></br></div>
   );};
